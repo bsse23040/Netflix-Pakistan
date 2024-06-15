1e4751f6459b917be77c7448440d2cd8567472d3
bsse23040 <143793936+bsse23040@users.noreply.github.com>
2024-06-15 19:01:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
+console.log(alert("Kindly use this site only on PC"))
 console.log(alert("Netflix Pakistan directly belongs to Aujla Studio, so Netflix World is requested to create its new unique layout. Otherwise it will face strict actions by MRAujla (CEO of Pak Aujla Studios)"));
